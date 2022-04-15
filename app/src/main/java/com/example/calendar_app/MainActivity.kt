e109506d556ebdd538b124c57b785a6efcdabd42
damirbaisalov <baisalovdamir@gmail.com>
2022-04-15 12:50:27 +0600

    added material-calendarview

--- app/src/main/java/com/example/calendar_app/MainActivity.kt
+++ app/src/main/java/com/example/calendar_app/MainActivity.kt
@@ -1,24 +1,25 @@
 package com.example.calendar_app
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.Window
 import android.view.WindowManager
 import android.widget.Button
-import android.widget.CalendarView
 import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import com.example.calendar_app.models.Database
 import com.example.calendar_app.models.DayData
+import com.prolificinteractive.materialcalendarview.CalendarDay
+import com.prolificinteractive.materialcalendarview.MaterialCalendarView
 import java.text.DateFormat
 import java.text.SimpleDateFormat
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var calendarView: CalendarView
+    private lateinit var calendarView: MaterialCalendarView
     private lateinit var textView: TextView
     private lateinit var currentDateTextView: TextView
     private lateinit var button: Button
@@ -27,11 +28,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         setLocale()
-
         init()
-
         getCurrentDate()
 
         button.setOnClickListener {
@@ -46,17 +44,22 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
-        calendarView.setOnDateChangeListener { view, year, month, dayOfMonth ->
+        calendarView.setOnDateChangedListener { widget, date, selected ->
 
-            val message = dayOfMonth.toString() + " " + (month+1)
-            for (day in list) {
-                if (message==day.date)
-                {
-                    showToast(day.description)
+            Log.e("EEE", date.month.toString())
+
+            if (!selected) {
+                widget.setDateSelected(date,true)
+
+                for (day in list) {
+                    if ((date.month == day.month.toInt()-1) && (date.day == day.day.toInt()))
+                        showToast(day.description)
                 }
+            } else {
+                widget.setDateSelected(date,false)
+                showToast("NONE")
             }
         }
-
     }
 
     private fun init() {
@@ -65,37 +68,34 @@ class MainActivity : AppCompatActivity() {
         textView = findViewById(R.id.text_view)
         currentDateTextView = findViewById(R.id.current_date_text_view)
         button = findViewById(R.id.button)
+
+        calendarView.selectionMode = MaterialCalendarView.SELECTION_MODE_MULTIPLE
+        setDates()
+        calendarView.setAllowClickDaysOutsideCurrentMonth(true)
     }
 
-//    private fun changeColorOfDates() {
-//        val calendar = Calendar.getInstance()
-//        for (day in list) {
-//            if (calendarView.date==day.date)
-//            {
-//                showToast(calendar.get(0).toString())
-//                calendarView.dateTextAppearance = R.style.CalenderViewWeekCustomText
-//            }
-//        }
-//
-//
-//    }
+    private fun setDates() {
+
+        for (day in list) {
+            for (y in 2015 until 2025) {
+                val calendar = Calendar.getInstance()
+                val monthInt = day.month.toInt()-1
+                val dayInt = day.day.toInt()
+                calendar[y, monthInt] = dayInt
+                calendarView.setDateSelected(calendar, true)
+            }
+        }
+    }
 
     private fun getCurrentDate() {
-        val selectedDate = calendarView.date
+        val selectedDate = CalendarDay.today()
         val calendar = Calendar.getInstance()
-        calendar.timeInMillis = selectedDate
+        calendar.timeInMillis = selectedDate.date.time
         val dateFormatter = DateFormat.getDateInstance(DateFormat.LONG, Locale("kk"))
         currentDateTextView.text = "${dateFormatter.format(calendar.time)}"
     }
 
     private fun setLocale() {
-
-//        val language = "kk"
-//        val locale = Locale(language)
-//        Locale.setDefault(locale)
-//        val config = Configuration()
-//        config.locale = locale
-//        baseContext.resources.updateConfiguration(config, baseContext.resources.displayMetrics)
 
         val locale = Locale("kk")
         val res = resources
--- app/src/main/java/com/example/calendar_app/models/DayData.kt
+++ app/src/main/java/com/example/calendar_app/models/DayData.kt
@@ -1,11 +1,14 @@
 package com.example.calendar_app.models
 
 import android.os.Parcelable
+import com.prolificinteractive.materialcalendarview.CalendarDay
 import kotlinx.android.parcel.Parcelize
 
 @Parcelize
 data class DayData(
     var id: String,
     var date: String,
+    var month: String,
+    var day: String,
     var description: String
 ): Parcelable
